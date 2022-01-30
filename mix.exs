1f9009c6dda77814623f3a30de8ea6d8ecdd3bc0
Jeroen Visser <jeroenvisser101@gmail.com>
2022-01-30 23:52:02 +0100

    Bump version to v0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule GrowthBook.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @repo_url "https://github.com/jeroenvisser101/growthbook-elixir"
 
   def project do
